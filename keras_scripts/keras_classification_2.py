2d128f3f7f1c40c9f747a4ff40b2d7b5147e6328
Marco <marcolerena@gmail.com>
2020-06-23 21:18:43 +0200

    Tested on unseen animals and added predict to captcha

--- keras_scripts/keras_classification_2.py
+++ keras_scripts/keras_classification_2.py
@@ -37,13 +37,13 @@ from sklearn.model_selection import train_test_split
 X = X.astype('float32')
 X /= 255
 
-X_train, X_val, y_train, y_val = train_test_split(X, y, train_size = 0.6, test_size = 0.4)
-X_val, X_test, y_val, y_test = train_test_split(X_val, y_val, train_size = 0.5, test_size = 0.5)
+X_train, X_val, y_train, y_val = train_test_split(X, y, train_size = 0.8, test_size = 0.2)
+# X_val, X_test, y_val, y_test = train_test_split(X_val, y_val, train_size = 0.5, test_size = 0.5)
 
 # convert class vectors to binary class matrices
 y_train = keras.utils.to_categorical(y_train, num_classes)
 y_val = keras.utils.to_categorical(y_val, num_classes)
-y_test = keras.utils.to_categorical(y_test, num_classes)
+# y_test = keras.utils.to_categorical(y_test, num_classes)
 
 
 #%%
@@ -52,6 +52,7 @@ import matplotlib.pyplot as plt
 
 plt.imshow(X_train[0,:,:,0], cmap='gray')
 
+#%%
 
 input_shape = (48, 48, 1)
 
@@ -82,3 +83,39 @@ score = model.evaluate(X_test, y_test, verbose = 0)
 
 print('Test loss:', score[0])
 print('Test accuracy:', score[1])
+
+#%%
+
+keras.models.save_model(model, 'model_keras_classif_2')
+
+#%%
+
+# TODO: Unseen animal test set
+
+# X_unseen = []
+# y_unseen = []
+
+
+# for path, label in [('./data/unseen_animals/clock/', 1), ('./data/unseen_animals/anti/', -1), ('./data/unseen_animals/done/', 0)]:
+#     for f in listdir(path):
+#         img = cv2.imread(join(path, f))
+#         img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+#         img = img[200:550,275:625]
+#         img = cv2.resize(img, (48,48))
+#         img = np.expand_dims(img, axis=2)
+#         X_unseen.append(img)
+#         y_unseen.append(label)
+# X_unseen = np.array(X_unseen)
+# y_unseen = np.array(y_unseen)
+
+# X_unseen = X_unseen.astype('float32')
+# X_unseen /= 255
+
+# # convert class vectors to binary class matrices
+# y_unseen = keras.utils.to_categorical(y_unseen, num_classes)
+#%%
+
+# score_unseen = model.evaluate(X_unseen, y_unseen, verbose = 0)
+
+# print('Test loss:', score_unseen[0])
+# print('Test accuracy:', score_unseen[1])
